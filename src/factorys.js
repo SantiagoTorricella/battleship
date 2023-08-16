cc3663ff04d781e4fea7acfa2125495af1f1b7a3
Santiago Torricella <torrisantiago@gmail.com>
2023-08-16 15:31:42 -0300

    Added functions to the board getGameBoard,checkCollision and checkValidPlacement for placing purpose

--- src/factorys.js
+++ src/factorys.js
@@ -1,6 +1,10 @@
+/* eslint-disable prefer-const */
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
+let rows = 10;
+let cols = 10;
 
+// factory function for ships
 function shipFactory(length) {
   return {
     length: length,
@@ -18,8 +22,50 @@ function shipFactory(length) {
   };
 }
 
-function gameBoardFactory() {
-  return {};
+// class for creating gameboard
+class GameBoard {
+  constructor() {
+    this.gameBoardArray = this.createGameBoard();
+    this.missedAttacks = [];
+  }
+  createGameBoard() {
+    let array = [];
+    let arrayItem = [];
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        arrayItem.push({ shipName: undefined, shipIndex: undefined });
+      }
+      array.push(arrayItem);
+      arrayItem = [];
+    }
+    return array;
+  }
+  getGameBoard() {
+    return this.gameBoardArray;
+  }
+  checkCollision(length, x, y, isHorizontal) {
+    if (isHorizontal) {
+      for (i = x; i < x + length; i++) {
+        if (this.gameBoardArray[i][y].shipName !== undefined) return true;
+      }
+    }
+    if (!isHorizontal) {
+      for (i = y; i < y + length; i++) {
+        if (this.gameBoardArray[x][i].shipName !== undefined) return true;
+      }
+    }
+    return false;
+  }
+  checkValidPlacement(length, x, y, isHorizontal) {
+    if (this.checkCollision()) return false;
+    if (x > 10 || x < 0 || y > 10 || y < 0) return false;
+    if (length + x > 10 && isHorizontal === true) return false;
+    if (length + y > 10 && isHorizontal === false) return false;
+    return true;
+  }
 }
 
-export default shipFactory;
+let newBoard = new GameBoard();
+console.log(newBoard.gameBoardArray);
+
+export { shipFactory, GameBoard };
--- src/tests/mainTest.test.js
+++ src/tests/mainTest.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-const */
 
-import shipFactory from "../factorys.js";
+import { shipFactory } from "../factorys.js";
+import { GameBoard } from "../factorys.js";
 
 test("length of ship is 3", () => {
   const ship = shipFactory(3);
@@ -18,4 +19,17 @@ test("ship is sunk", () => {
   ship.hit();
   ship.hit();
   expect(ship.isSunk()).toBe(true);
+});
+
+describe("board", () => {
+  test("the board is created", () => {
+    let boardGame = new GameBoard();
+    let object = { ship: undefined, shipIndex: undefined };
+    expect(boardGame.gameBoardArray[0][0]).toEqual(object);
+  });
+  test("cant place invalid ships", () => {
+    let ship = shipFactory(4);
+    let board = new GameBoard();
+    expect(board.checkValidPlacement(ship.length, 8, 5, true)).toBe(false);
+  });
 });
