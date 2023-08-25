4022aadcf9596c47e48e7eec4b6ad6c35454a25d
Santiago Torricella <torrisantiago@gmail.com>
2023-08-24 21:47:59 -0300

    Added grid for the boards Boards and grid created for the game

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import { shipFactory } from "./factorys.js";
 import { playerFactory } from "./factorys.js";
 import { GameBoard } from "./factorys.js";
 import "./styles/style.css";
+
+function createBoard(boardName) {
+  const playerBoard = document.querySelector(`.${boardName}`);
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.setAttribute("x", i);
+      cell.setAttribute("y", j);
+      playerBoard.appendChild(cell);
+    }
+  }
+}
+createBoard("player-board");
+createBoard("ai-board");
